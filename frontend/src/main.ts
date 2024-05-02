33373646773932768eed7e81a9650f9e696e0435
Li <li.hvktqs@gmail.com>
2024-05-02 22:16:04 +0700

    feat: enable vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,21 @@
+// Assets
 import './assets/main.css'
+import 'vuetify/styles'
 
+// Main components
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
 
+// Routers
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(createVuetify())
+
 app.use(router)
 
 app.mount('#app')
